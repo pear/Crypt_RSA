f3b4cd486a2d6a939c3ef2ffeb5f0d3bcfe8d7fb
Daniel O'Connor <clockwerx@php.net>
2009-07-18 09:21:48 +0000

    Re-organisation of directory structure to make unit tests work, adding unit tests
    git-svn-id: http://svn.php.net/repository/pear/packages/Crypt_RSA/trunk@284299 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -31,28 +31,32 @@
  */
 
 require_once 'Crypt/RSA.php';
+require_once 'PHPUnit/Framework/TestCase.php';
 
-echo "Start of testing factory() methods...\n";
+class Crypt_RSA_FactoryTest extends PHPUnit_Framework_TestCase {
 
-// try to create a Crypt_RSA object using factory() static call
-$obj = &Crypt_RSA::factory();
-if (PEAR::isError($obj)) {
-    echo 'error in Crypt_RSA factory(): ', $obj->getMessage(), "\n";
+    // try to create a Crypt_RSA object using factory() static call
+    public function testShouldMakeAnObject() {
+        $obj = Crypt_RSA::factory();
+
+        $this->assertFalse(PEAR::isError($obj), 'error in Crypt_RSA factory()');
+    }
+
+    public function testShouldCreateAKeyPair() {
+        $obj = Crypt_RSA_KeyPair::factory(128);
+
+        $this->assertFalse(PEAR::isError($obj), 'error in Crypt_RSA_KeyPair factory(): ');
+    }
+
+    public function testShouldMakeAKey() {
+        $obj = Crypt_RSA_KeyPair::factory(128);
+        $key = $obj->getPrivateKey();
+
+        // try to create a Crypt_RSA_Key object using factory() static call
+        $obj = Crypt_RSA_Key::factory($key->getModulus(), $key->getExponent(), $key->getKeyType());
+
+        $this->assertFalse(PEAR::isError($obj), 'error in Crypt_RSA_Key factory(): ');
+    }
+
 }
 
-// try to create a Crypt_RSA_KeyPair object using factory() static call
-$obj = &Crypt_RSA_KeyPair::factory(128);
-if (PEAR::isError($obj)) {
-    echo 'error in Crypt_RSA_KeyPair factory(): ', $obj->getMessage(), "\n";
-}
-$key = $obj->getPrivateKey();
-
-// try to create a Crypt_RSA_Key object using factory() static call
-$obj = &Crypt_RSA_Key::factory($key->getModulus(), $key->getExponent(), $key->getKeyType());
-if (PEAR::isError($obj)) {
-    echo 'error in Crypt_RSA_KeyPair factory(): ', $obj->getMessage(), "\n";
-}
-
-echo "end\n";
-
-?>
